006941afa0f3146f912f383e5750624bf9e162ae
Fareya Azam <113286085+fareya22@users.noreply.github.com>
2025-01-01 22:09:47 +0600

    Delete Frontend directory

--- Frontend/utils/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:3000' });
-
-export const createRandomEnvironment = () => API.post('/environment/create-random');
-export const moveAgent = (data) => API.post('/agent/move', data);
