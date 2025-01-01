3205a0cd80d99a9ae5f79d4cbe7b3c1e6e5fd6ef
Fareya Azam <113286085+fareya22@users.noreply.github.com>
2025-01-01 22:09:30 +0600

    Delete Backend directory

--- Backend/routes/agentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { makeMove } = require('../controllers/agentController');
-const router = express.Router();
-
-router.post('/move', makeMove);
-
-module.exports = router;
--- Backend/routes/environmentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createRandomEnvironment } = require('../controllers/environmentController');
-const router = express.Router();
-
-router.post('/create-random', createRandomEnvironment);
-
-module.exports = router;
